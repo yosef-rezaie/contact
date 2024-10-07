ef071a59f101e8deab60cc329abed14d9107c380
Yosef Rezaie <yosefrezaie5001@gmail.com>
2024-10-07 10:57:57 +0330

    css.module.css except contacts was styles

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import ContactsList from "./ContactsList";
+import styles from "./Contacts.module.css";
 import { v4 } from "uuid";
 
 const inputs = [
@@ -55,8 +56,8 @@ function Contacts() {
   };
 
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <div className={styles.form}>
         {inputs.map((input, index) => (
           <input
             key={index}
@@ -69,7 +70,7 @@ function Contacts() {
         ))}
         <button onClick={addHandler}>Add Contact</button>
       </div>
-      <div>{alert && <p>{alert}</p>}</div>
+      <div className={styles.alert}>{alert && <p>{alert}</p>}</div>
       <ContactsList contacts={contacts} deleteHandler={deleteHandler} />
     </div>
   );
